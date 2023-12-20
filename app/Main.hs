ef1d3393645fff25ec5c1e6bdb0b998865c33d64
NzSN <nzsn0101@gmail.com>
2023-12-20 17:34:25 +0800

    Handle commandline arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 module Main (main) where
 
-import Lib
+import System.Console.CmdArgs
+
+data Arguments = Arguments { input :: String,
+                             output :: String }
+                deriving (Show, Data, Typeable)
+
+arguments = Arguments{input = "grammar.txt" &= help "Path of input grammar file",
+                      output = "grammar_output.txt" &= help "Path of output grammar file"}
 
 main :: IO ()
-main = someFunc
+main = do
+  -- Parse commandline arguments
+  args <- cmdArgs arguments
