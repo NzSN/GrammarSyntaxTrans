c77629b5fa9add064d67688c908f106dbb948dcb
NzSN <nzsn0101@gmail.com>
2023-12-21 12:06:45 +0800

    Split functions in main into where.

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,16 @@ module Main (main) where
 import System.IO
 import System.Console.CmdArgs
 
-import GrammarParser (parseGrammar)
+import GrammarParser (parseGrammar, Rule)
 import TargetGrammar.AntlrGrammar (AntlrRepr(AntlrRepr))
 
-data Arguments = Arguments { input  :: String,
-                             output :: String,
-                             debug  :: Bool}
+type Path = String
+data Arguments = Arguments { input  :: !String,
+                             output :: !String,
+                             debug  :: !Bool}
                 deriving (Show, Data, Typeable)
 
+arguments :: Arguments
 arguments = Arguments{input = "grammar.txt" &= help "Path of input grammar file",
                       output = "grammar_output.txt" &= help "Path of output grammar file",
                       debug = False &= help "Debug mode"}
@@ -22,23 +24,26 @@ main :: IO ()
 main = do
   -- Parse commandline arguments
   args <- cmdArgs arguments
+  -- Parse input grammar file
+  parseFile (input args) >>=
+  -- Transform to Antlr form
+    asAntlr (output args)
+  where
+    parseFile :: Path -> IO (Maybe [Rule])
+    parseFile path = do
+      h <- openFile path ReadMode
 
-  h <- openFile (input args) ReadMode
-  out <- openFile (output args) WriteMode
+      contents <- hGetContents h
+      let rules = parseGrammar contents
+      case rules of
+        Nothing -> return Nothing
+        Just rs  -> return $ Just $ rs
 
-  grammar_contents <- hGetContents h
-
-  let rules = parseGrammar grammar_contents
-  case rules of
-    Nothing -> error "Failed to parse input grammar file"
-    Just rs -> do
-      let ruleRepr = AntlrRepr rs
-
-      if debug args
-        then do
-          out_rule <- openFile "debug.txt" WriteMode
-          hPrint out_rule rs >> hClose out_rule
-        else do
-          hPrint out ruleRepr >> hClose out
-
-  hClose h
+    asAntlr :: Path -> Maybe [Rule] -> IO ()
+    asAntlr path rsMaybe = do
+      case rsMaybe of
+        Nothing -> error "Nothing to transform (maybe fail to parse input file)"
+        Just rs -> do
+          h <- openFile path WriteMode
+          hPrint h $ AntlrRepr rs
+          hClose h
